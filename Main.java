165a24c6ebd9198cf2d143ac075a0cd40daf7a94
Carlos Oliveira <107328690+oCarlosE@users.noreply.github.com>
2025-02-27 14:26:13 +0000

    Third function

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ public class Main {
         teste = sumNumNat(3, 3);
         System.out.println("O valor da funcão 2 é: " + teste);
 
+        double value = sumDiv(2);
+        System.out.printf("O valor da terceira função é: %.2f \n", value);
+
     }
 
     //Multiplicação de dois números naturais, através de somas sucessivas (Ex.: 6 ∗ 4 = 4 + 4 + 4 + 4 + 4 + 4).
@@ -18,7 +21,7 @@ public class Main {
 
         return produto + multNumNat(multiplicador-1, produto);
     }
-    
+
     //Soma de dois números naturais, através de incrementos sucessivos (Ex.: 3 + 2 = + + (+ + + 1)).
     public static int sumNumNat(int parcela, int total){
         if (parcela == 0) {
@@ -26,5 +29,14 @@ public class Main {
         }
 
         return sumNumNat(parcela -1, total +1);
+    }
+
+    //Cálculo de 1 + 1/2 + 1/3 + 1/4 + ... + 1/N
+    public static double sumDiv(int divisor){
+        if (divisor == 1) {
+            return 1.0;
+        }
+        
+        return 1.0 / divisor + sumDiv(divisor - 1);
     }
 }
